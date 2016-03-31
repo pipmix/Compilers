1564e7176bff76195f67d450bcd2e913fc58421b
pescobedo <onloadx@yahoo.com>
2016-03-30 18:32:32 -0700

    test

---
+++ hw3/main.cpp
@@ -0,0 +1,24 @@
+
+//	**** PROGRAM BY 
+
+//	Matthew Escobedo
+//	Alexandra Woods
+
+//	****
+
+
+#include <iostream>
+using namespace std;
+
+extern int yyparse();
+extern void yyerror(const char *);
+extern int yylex();
+
+
+int main(){
+
+	int result = yyparse();
+	return 0;
+
+
+}
